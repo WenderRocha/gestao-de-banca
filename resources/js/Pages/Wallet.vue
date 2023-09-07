f7dc9edc83ca2fd46d9c0098c1374abbe2398551
Wender Rocha <wender_dev@hotmail.com>
2023-09-07 14:22:55 -0300

    criado componente de carteira

--- resources/js/Pages/Wallet.vue
+++ resources/js/Pages/Wallet.vue
@@ -1,16 +1,17 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
-import Button from '@/Components/Button.vue'
-import { GithubIcon } from '@/Components/Icons/brands'
+import AuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import Button from "@/Components/Button.vue";
+import { GithubIcon } from "@/Components/Icons/brands";
+import Wallet from "../Pages/Components/Wallet/Wallet.vue";
 </script>
 
 <template>
     <AuthenticatedLayout title="Carteiras">
         <template #header>
-            <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
-                <h2 class="text-xl font-semibold leading-tight">
-                    Carteiras
-                </h2>
+            <div
+                class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between"
+            >
+                <h2 class="text-xl font-semibold leading-tight">Carteiras</h2>
 
                 <Button
                     external
@@ -27,8 +28,14 @@ import { GithubIcon } from '@/Components/Icons/brands'
             </div>
         </template>
 
-        <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-            You're logged in!
+        <div
+            class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1"
+        >
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-3">
+                <Wallet />
+                <Wallet />
+                <Wallet />
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
