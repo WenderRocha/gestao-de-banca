dab17493e4b01fea3644cc22b9ea9007605e29ac
Wender Rocha <wender_dev@hotmail.com>
2023-09-25 13:41:30 -0300

    Criado componente de card para gerênciamento

---
+++ resources/js/Pages/Components/Shared/ManagementCard.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { defineProps } from "vue";
+import CurrencyFormat from "./CurrencyFormat.vue";
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'Title'
+    },
+    balance:  {
+        type: Number,
+        default: 100
+    },
+    currency: {
+        type: String,
+        default: 'BRL'
+    },
+});
+</script>
+
+<template>
+    <a
+        href="#"
+        class="max-w-sm p-6 bg-purple-500 border border-gray-200 rounded-lg shadow dark:bg-purple-800 dark:border-gray-700 dark:hover:bg-purple-700 flex justify-between"
+    >
+        <div>
+            <h5
+                class="mb-2 text-1xl font-bold tracking-tight text-white dark:text-white uppercase"
+            >
+                {{ props.title }}
+            </h5>
+            <div class="flex items-center gap-1">
+                <!-- <small class="font-bold text-white dark:text-gray-400"
+                    >R$</small
+                >
+                <p class="font-bold text-2xl text-white dark:text-gray-400">
+                    100,00
+                </p> -->
+
+                <p class="font-bold text-2xl text-white dark:text-gray-400">
+                    <CurrencyFormat :currency="props.currency" :balance="props.balance"/>
+                </p>
+
+                
+            </div>
+        </div>
+        <div>
+            <slot name="icon">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-10 text-white"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3"
+                    />
+                </svg>
+            </slot>
+        </div>
+    </a>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,16 +1,18 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
-import Button from '@/Components/Button.vue'
-import { GithubIcon } from '@/Components/Icons/brands'
+import AuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import Button from "@/Components/Button.vue";
+import { GithubIcon } from "@/Components/Icons/brands";
+import { Progress } from "flowbite-vue";
+import ManagementCard from "../Pages/Components/Shared/ManagementCard.vue";
 </script>
 
 <template>
     <AuthenticatedLayout title="Dashboard">
         <template #header>
-            <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
-                <h2 class="text-xl font-semibold leading-tight">
-                    Dashboard
-                </h2>
+            <div
+                class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between"
+            >
+                <h2 class="text-xl font-semibold leading-tight">Dashboard</h2>
 
                 <Button
                     external
@@ -27,8 +29,40 @@ import { GithubIcon } from '@/Components/Icons/brands'
             </div>
         </template>
 
-        <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-            You're logged in!
+        <div
+            class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1"
+        >
+
+            <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
+                <ManagementCard title="Saldo inicial" currency="USD" :balance="250"/>
+                <ManagementCard title="Saldo Atual" currency="USD" :balance="250">
+                    <template #icon>
+               
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                        </svg>
+
+                    </template>
+                </ManagementCard>
+                <ManagementCard title="Take profit" currency="USD" :balance="250">
+                    <template #icon>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                        </svg>
+                 
+                    </template>
+                </ManagementCard>
+                <ManagementCard title="Stop Loss" currency="USD" :balance="250">
+                    <template #icon>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                        </svg>
+                    </template>
+                </ManagementCard>
+            </div>
+            
+
+       
         </div>
     </AuthenticatedLayout>
 </template>
